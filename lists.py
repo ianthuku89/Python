57829c657690f736c60ecf026b18c353c57c6645
ianthuku89 <111960689+ianthuku89@users.noreply.github.com>
2024-01-09 18:33:21 +0300

    Changes made to lists.py to show how to create and call a function

---
+++ lists.py
@@ -0,0 +1,8 @@
+#Creating a function that says Hi
+#While adding example parameters
+def say_hi(name, age):
+  print("Hello " + name + " you are " + age)
+#Call the funcction
+say_hi("Ian", "21")
+
+
