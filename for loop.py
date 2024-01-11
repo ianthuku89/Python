d5aabf0fbf693e20f1ec5fc9787f74abb1898359
ianthuku89 <111960689+ianthuku89@users.noreply.github.com>
2024-01-11 20:11:13 +0300

    simple for loops

---
+++ for loop.py
@@ -0,0 +1,13 @@
+for letter in "Giraffe Academy":
+  #for every letter you want to perform action below
+  print(letter)
+
+friends = ["Ian", "Melissa", "Doris"]
+for name in friends:
+  print(name)
+#prints out all the names in the array friends
+
+for index in range(10):
+  print(index)
+#prints every no in specified range excluding 10
+
