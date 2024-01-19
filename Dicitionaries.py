f82fea4f4b59d6aa6bd66210cbefacc82790cc3e
ianthuku89 <111960689+ianthuku89@users.noreply.github.com>
2024-01-19 18:12:39 +0300

    Dictionaries

--- Dicitionaries.py
+++ Dicitionaries.py
@@ -14,3 +14,38 @@ monthConversions = {
 }
 
 print(monthConversions.get("Apr"))
+#Creating a dictionary with multiple values
+from collections import defaultdict
+d = defaultdict(list)
+d['a'].append(1)
+d['a'].append(2)
+d['b'].append(4)
+d['b'].append(5)
+
+print(d.get('b'))
+
+#Keeping Dictionaries in order
+from collections import OrderedDict
+d = OrderedDict()
+d['Ian'] = 1
+d['Duncan'] = 2
+d['Tira'] = 3
+d['Thuku'] = 4
+
+print(d.get('Ian'))
+
+#Calculating with Dictionaries
+prices = {
+ 'ACME': 45.23,
+ 'AAPL': 612.78,
+ 'IBM': 205.55,
+ 'HPQ': 37.20,
+ 'FB': 10.75
+}
+min_price = min(zip(prices.values(), prices.keys()))
+max_price = max(zip(prices.values(), prices.keys()))
+print(min_price,max_price)
+
+#Ranking the data above
+prices_sorted = sorted(zip(prices.values(), prices.keys()))
+print(prices_sorted)
