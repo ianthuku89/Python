25c19857947275d24eb98da65c4944a444525860
ianthuku89 <111960689+ianthuku89@users.noreply.github.com>
2024-01-21 20:30:13 +0300

    Mapping

---
+++ Mapping.py
@@ -0,0 +1,5 @@
+#Mapping Names to sequence elements
+from collections import namedtuple
+Subscriber = namedtuple('Subscriber', ['addr', 'joined'])
+sub = Subscriber('jonesy@example.com','2012-10-19')
+print(sub.joined)
