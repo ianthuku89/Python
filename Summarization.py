7efe8ebdf03d7f9298b0642ff61e5f1c9a4d6516
ianthuku89 <111960689+ianthuku89@users.noreply.github.com>
2024-03-04 18:45:36 +0300

    Working with csv file

--- Summarization.py
+++ Summarization.py
@@ -12,3 +12,9 @@ print(Year['Data.Temperature.Avg Temp'].value_counts()[:10])
 #Group by month
 date = Year.groupby('Date.Month')
 print(len(date))
+#Determine counts on each month
+date_Count = date.size()
+print(date_Count[0:10])
+#Sort the counts in descending order
+date_Count_sorted = date_Count.sort_values(ascending=False)
+print(date_Count_sorted.head()) 
